201c8346be24b1fa4635296a624ec758c77c6b67
Melo <mee-lensey_louis26@milton.edu>
2023-11-10 13:23:21 -0500

    Initial commit

---
+++ src/MadLib.java
@@ -0,0 +1,96 @@
+public class MadLib {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        MadLib myStory = new MadLib();
+
+    }//main method
+
+    public MadLib(){
+        System.out.println("halo world");
+
+        String classmate= "myDorm";
+        System.out.println("myDorm");
+        String city= "Plymouth";
+        System.out.println("Plymouth");
+        String noun = "Water";
+        System.out.println("Water");
+        String adjective= ("nice");
+        System.out.println("nice");
+        String pluralNoun= "Bags";
+        System.out.println("Bags");
+        String pluralAnimal=("Bears");
+        System.out.println("Bears");
+        String trueOrFalse=("True");
+        System.out.println("True");
+        Float decimalBiggerThan1 =(95.6f);
+        System.out.println(95.6f);
+        final int number1;
+        number1=5;
+        System.out.println(number1);
+        int number2;
+        number2=3;
+        System.out.println(number2);
+        int wholeNumberBetween1And4;
+        wholeNumberBetween1And4=2;
+        System.out.println(wholeNumberBetween1And4);
+        String miltonTeacher =("Mr.Jordan");
+        System.out.println("Mr.Jordan");
+        String miltonDean=("Mr.Ruiz");
+        System.out.println("Mr.Ruiz");
+        int letterGrade;
+        letterGrade=100;
+        System.out.println(letterGrade);
+        String season= ("Spring");
+        System.out.println("Spring");
+        //define a string called story for mad lib
+        String story = "This weekend I am going camping with "
+                +classmate+ ". We are going to a campsite in "
+                +city+ ". I packed my "+noun+ " and "
+                +pluralNoun + ".  The forecast is calling for a high of "
+                +decimalBiggerThan1+ " degrees fahrenheit. So it should be a "
+                +adjective+" day! This year, the park rangers have seen "
+                +number1+ " " +pluralAnimal + " which seems kind of dangerous. "
+
+                +miltonTeacher+" said it's " + trueOrFalse + " that "+pluralAnimal+ " eat class "
+                +wholeNumberBetween1And4+ " students in the " +season+ ". But, "
+                +miltonDean+ " said it actually has only happened "
+                + number2+" times. Wish us luck! If we survive, we will earn a "
+                +letterGrade+ "- in P.E. class.";
+
+//now print story to the dos window
+        System.out.println(story);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //define a string called story for mad lib
+
+
+//now print story to the dos window
+
+
+
+
+    }//constructor method
+}
+
